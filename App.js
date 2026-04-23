742b9685c2d84243d8d10ba435f10b2a9bd72bf9
Eduardo Costa01 <eccosta2000@bol.com.br>
2026-04-23 17:34:44 -0300

    converse de moedas

--- App.js
+++ App.js
@@ -1,20 +1,143 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, KeyboardAvoidingView, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Button } from './src/components/Button';
+import { styles } from './App.styles'
+import { currencies } from './src/constants/currencies'
+import { Imput } from './src/components/Input';
+import { ResultCard } from './src/components/ResultCard';
+import { useState } from 'react';
+import {exchangeRateApi} from './src/services/api'
+import {convertCurrency} from './src/utils/convertCurrency'
+
 
 export default function App() {
+  
+  const [ amount, setAmount] = useState('')
+  const [ fromCurrency, setFromCurrency] = useState('USD')
+  const [ toCurrency, setToCurrency] = useState('BRL')
+  const [ result, setResult] = useState('')
+  const [ loading, setLoading] = useState(false)
+  const [ exchangeRate, setExchangeRate] = useState(null)
+
+  async function fetchExchanRate() {
+
+    try {
+
+      setLoading(true)
+      
+      if (!amount) return
+      
+      const data = await exchangeRateApi(fromCurrency)
+      const rate = data?.rates?.[toCurrency]
+      if (!rate) return
+      
+      setExchangeRate(rate)
+      
+      const convertedAmount = convertCurrency(amount, rate)
+      setResult (convertedAmount)
+
+    } catch(err) {
+     
+      alert("Erro, tente novamente")
+
+    } finally {
+
+      setLoading(false)
+    }
+  } 
+
+  function swapCurrency() {
+    setFromCurrency(toCurrency)
+    setToCurrency(fromCurrency)
+    setResult('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+
+      <ScrollView style={styles.scrollView}>
+
+          <View style={styles.content}>
+            
+            <StatusBar style="light" />
+
+            <View style={styles.header}>
+              
+              <Text style={styles.title}>Conversor de Moedas</Text>
+
+              <Text style={styles.subTitle}>
+                Converta valores em diterentes moedas
+              </Text>
+            </View>
+
+            <View style={styles.card}>
+              
+              <Text style={styles.label}>De:</Text>
+
+              <View style={styles.currencyGrid}>
+                {currencies.map(currency => (
+                  <Button variant='primary'
+                    key={currency.code}
+                    currency={currency}
+                    onPress={() => setFromCurrency(currency.code)}
+                    isSelected={fromCurrency === currency.code}
+                  ></Button>
+                ))}
+              </View>
+                
+              <Imput label="Valor:" value={amount} onChangeText={setAmount}/>
+
+              <TouchableOpacity style={styles.swapButton} onPress={swapCurrency}>
+                <Text style={styles.swapButtonText}>
+                  ↑↓
+                </Text>
+              </TouchableOpacity>
+
+              <Text style={styles.label}>Para:</Text>
+
+              <View style={styles.currencyGrid}>
+                {currencies.map(currency => (
+                  <Button variant='secondary'
+                    key={currency.code}
+                    currency={currency}
+                    onPress={() => setToCurrency(currency.code)}
+                    isSelected={toCurrency === currency.code}
+                  ></Button>
+                ))}
+              </View>
+            </View>
+            
+            <TouchableOpacity 
+            style={[styles.convertButton, (!amount || loading && styles.convertButtonDisabled)]}
+            onPress={fetchExchanRate}
+            disabled={!amount || loading}
+            >
+              {loading ? (
+                <ActivityIndicator color='white'/>
+              ) : (
+              <Text style={styles.swapButtonText}>
+                Converter
+              </Text>
+
+              )}
+            </TouchableOpacity>
+            
+            <ResultCard
+              amount={amount}
+              exchangeRate= {exchangeRate}
+              result = {result}
+              fromCurrency = {fromCurrency}
+              toCurrency = {toCurrency}
+            />
+          
+          </View>
+
+      </ScrollView>
+    
+    </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
